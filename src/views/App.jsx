2c107ac2ac8aa3b7487bed8e1f6871c3989009e0
safdar-azeem <67892800+safdar-azeem@users.noreply.github.com>
2021-12-15 01:40:17 +0500

    initial commit

---
+++ src/views/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom';
+import {Routes} from '../routes'
+
+const App = () => {
+    return (
+			<div className="pt-9">
+			<header className="text-center">
+				<h1 className="fs-39">CARD  GAME</h1>
+			</header>
+				<Switch>
+					{Routes.map((route, idx) => (
+						<Route
+							key={idx}
+							exact={route.exact}
+							path={route.path}
+							component={route.component}
+						/>
+					))}
+				</Switch>
+			</div>
+		);
+}
+
+export default App
